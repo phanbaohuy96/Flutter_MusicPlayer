29fdcb271c043a07a2a7374573aff91967939dcb
phanbaohuy96 <2001140087@ittc.edu.vn>
2019-04-25 23:05:35 +0700

    - Request permission for android

--- android/app/src/main/java/com/example/music_player/MainActivity.java
+++ android/app/src/main/java/com/example/music_player/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.music_player;
 
 import android.os.Bundle;
+import android.content.pm.PackageManager;
+import android.Manifest;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -9,5 +14,15 @@ public class MainActivity extends FlutterActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+  }
+
+  @Override 
+  protected void onResume()
+  {
+    super.onResume();
+    if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+    {
+      ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1000);
+    }
   }
 }
